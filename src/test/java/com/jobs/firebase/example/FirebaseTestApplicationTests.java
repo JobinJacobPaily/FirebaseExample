e14075a0683f2ce477eb865965c4a5f88f714e75
Jobin Jacob Paily <jobinjacobpaily@gmail.com>
2022-04-06 20:18:30 +0530

    Initial Commit

---
+++ src/test/java/com/jobs/firebase/example/FirebaseTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobs.firebase.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirebaseTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
